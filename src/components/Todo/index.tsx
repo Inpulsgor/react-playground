4d1e2d46cd1f468864910fdad023e0d49f0c65fa
Inpuls <c4.tm.gor@gmail.com>
2021-10-18 19:59:53 +0300

    test prettier

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import { TodoList } from '../';
 import initialTodos from './todos.json';
 import { IProps, IState } from './interface';
-// import styles from './styles.module.scss';
+import styles from './styles.module.scss';
 
 class Todo extends Component<IProps, IState> {
   state = {
@@ -17,13 +17,19 @@ class Todo extends Component<IProps, IState> {
 
   render() {
     const { todos } = this.state;
+    const completedTodos = todos.filter(todo => todo.completed);
 
     return (
-      <>
+      <div className={styles.todos}>
+        <span className={styles.total}>Total: {todos.length}</span>
+        <span className={styles.completed}>
+          Completed: {completedTodos.length}
+        </span>
+
         {todos.length > 0 && (
           <TodoList todos={todos} onDeleteTodo={this.handleDelete} />
         )}
-      </>
+      </div>
     );
   }
 }
