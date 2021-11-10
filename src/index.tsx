c045b4747960ef018a2af59222884b5ef4a82926
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-11-10 22:57:34 +0200

    react-router added to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'modern-normalize/modern-normalize.css';
 import './scss/main.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
   document.getElementById('root'),
 );
