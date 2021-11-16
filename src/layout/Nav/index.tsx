3e7fa6e38ea1b9972ba958c0fc21ce20c50e2730
Dmytro Smakovskyi <c4.tm.gor@gmail.com>
2021-11-16 14:46:50 +0200

    added active navlink class by pathname

--- src/layout/Nav/index.tsx
+++ src/layout/Nav/index.tsx
@@ -1,17 +1,27 @@
-import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import cn from 'classnames';
+import { useLocation } from 'react-router';
+import { NavLink } from 'react-router-dom';
 import routes from 'src/services/routes';
 import styles from './styles.module.scss';
 
-const Nav: FC = () => {
+const Nav = () => {
+  const { pathname } = useLocation();
+
   return (
     <nav className={styles.nav}>
       <ul className={styles.list}>
         {routes.length &&
-          routes.map(route => (
-            <Link key={route.label} to={route.path}>
-              {route.label}
-            </Link>
+          routes.map(({ path, label }) => (
+            <NavLink
+              key={label}
+              to={path}
+              className={cn(styles.link, {
+                [styles.active]: path === pathname,
+              })}
+            >
+              {label}
+            </NavLink>
           ))}
       </ul>
     </nav>
